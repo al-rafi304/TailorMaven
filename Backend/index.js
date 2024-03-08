9dcc0cf48d93953deb6dd2c05e35609e053a424e
Mohammad Al Rafi <arafi304@gmail.com>
2024-03-09 03:18:35 +0600

    BugFix: Google login searches with username now + Refactoring Passport.js
    Before it used to search with all the attributes of user objects which resulted in error if any one of the attributes were updated

--- Backend/index.js
+++ Backend/index.js
@@ -8,15 +8,17 @@ const app = express();
 const routes = require('./routes/router')
 const authRoutes = require('./routes/authRoutes');
 const authMiddleware = require('./milddleware/authMiddleware');
+const passportMiddleware = require('./milddleware/passport-config')
+
 const connectDB = require('./db/connect')             // Database Connection
 
 const port = process.env.PORT || 3000;
 
 app.use(express.json())
 
-app.use(authMiddleware.sessionMiddleware);
-app.use(authMiddleware.passportInitialize);
-app.use(authMiddleware.passportSession);
+app.use(passportMiddleware.sessionMiddleware);
+app.use(passportMiddleware.passportInitialize);
+app.use(passportMiddleware.passportSession);
 
 app.use('/api/v1', routes) 
 app.use('/auth', authRoutes);
