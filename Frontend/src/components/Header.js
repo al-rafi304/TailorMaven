6394c30a7fc764349a31bdada0da911d8d3e053e
farhan-sadik247 <fsadik2319@gmail.com>
2024-04-16 01:55:22 +0600

    cart,menu-bar,userprofile picture

--- Frontend/src/components/Header.js
+++ Frontend/src/components/Header.js
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import "./Header.css"
 import AuthAPI from '../services/AuthAPI';
+import MenuBar from './MenuBar';
 
 function Header() {
 
@@ -24,33 +25,53 @@ function Header() {
 
  return (
 	<header>
-	 <div className='dropdown-customsuit'>
-		<Link to = "/visualize"><button>Custom Suit</button></Link>
-		<div className='dropdown-content'>
-		 
-		</div>
-	 </div>
+	 <MenuBar/>
 	 <div className='logo-img'>
 		<Link to="/">
 		 <img src="logo_v2_transparent.png" alt="Website Logo"/>
 		</Link>
 	 </div>
-	 <div className='user-actions'>
+
+	<div className='user-actions'>
 		<div className='cart'>
-		 <i className="fa-solid fa-bag-shopping"></i>
+			<Link to='add-to-cart'>
+			<img className="addtocart" src="bag.png" alt="Cart"/>
+			</Link>
 		</div>
-			<div className="dropdown user-dropdown">
-				<button className="btn dropdown-toggle " type="button" data-bs-toggle="dropdown" aria-expanded="false">
-				<i className="fa-regular fa-user"></i>
-				</button>
-				<ul className="dropdown-menu user-dropdownmenu">
-					{!userId &&
-						<><li><Link className='btn btn-login' to="/login">Login</Link></li>
-						<li><Link className='btn btn-reg' to="/register">Register</Link></li></>}
-					{userId && <li><Link className='btn btn-signout' onClick = {handleLogOut}>Sign Out</Link></li>}
-				</ul>
+	{!userId && (
+		<div className="dropdown user-dropdown">
+		<button className="btn dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+			<i className="fa-regular fa-user"></i>
+		</button>
+		<ul className="dropdown-menu user-dropdownmenu">
+			<li><Link className='btn btn-login' to="/login">Login</Link></li>
+			<li><Link className='btn btn-reg' to="/register">Register</Link></li>
+		</ul>
+		</div>
+	)}
+	{userId && (
+		<div className="dropdown user-dropdown">
+		<button className="btn dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+			<img className="user-profile-pic" src="cat.jpg" alt="User Profile"/>
+		</button>
+		<ul className="dropdown-menu user-dropdownmenu">
+			<li>
+			<div className="user-info">
+				<img className="user-profile-pic-large" src="cat.jpg" alt="User Profile"/>
+				<div className="user-details">
+				<p className="user-name">Farhan Sadik</p>
+				<p className="user-email">md.farhan.sadik@g.bracu.ac.bd</p>
+				<div className="btn-signout" onClick={handleLogOut}>Sign Out</div>
+				</div>
 			</div>
-	 </div>
+			</li>
+		</ul>
+		</div>
+	)}
+	</div>
+
+
+
 	</header>
  );
 };
