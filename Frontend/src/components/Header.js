81e253f4389ffbc449a8b095edb20bbb1fdd04bf
Imteaz00 <imteaz969@gmail.com>
2024-04-19 23:21:52 +0600

    fabric filter

--- Frontend/src/components/Header.js
+++ Frontend/src/components/Header.js
@@ -40,11 +40,11 @@ const Header = (props) => {
 	 </div>
 
 	<div className='user-actions'>
-			<div className='admin-icon'>
+			{userInfo.isAdmin && <div className='admin-icon'>
                 <Link to='/admin'>
                 <img className="admin-img" src="admin.png" alt="admin-img"/>
                 </Link>
-            </div>
+            </div>}
         {userId ?
             <div className='cart'>
                 <Link to='add-to-cart'>
