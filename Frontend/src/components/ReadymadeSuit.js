1244ec54267c36a9137a9d27caa126fdb0241ba3
farhan-sadik247 <fsadik2319@gmail.com>
2024-04-20 21:10:14 +0600

    reg add and pho no and readymade suit css

--- Frontend/src/components/ReadymadeSuit.js
+++ Frontend/src/components/ReadymadeSuit.js
@@ -38,81 +38,10 @@ function ReadymadeSuit() {
                         </Link>
                     </div>
                 </article>))}
-
-                {/* <article className="card__article">
-                    <img src="/doublebreast.png" alt="image" className="card__img" />
-
-                    <div className="card__data">
-                        <span className="card__description">Suit measurements:</span>
-                        <span className="card__description">Length: 45,Waist: 45,Chest: 45,Arm Length: 45,Button Color: Blue.</span>
-                        <span className="card__description">Fabric: cotton</span>
-                        <span className="card__description">Suit Price: 999$</span>
-                        <Link to="add-to-cart">
-                        <img src="/shopping-cart.svg" alt="cart" className="readysuit-cart" />
-                        </Link>
-                    </div>
-                </article>
-
-                <article className="card__article">
-                    <img src="/doublebreast2.png" alt="image" className="card__img" />
-
-                    <div className="card__data">
-                    <h2 className="card__title">Doublebreast Suit</h2>
-                        <span className="card__description">Suit measurements:</span>
-                        <span className="card__description">Length: 45,Waist: 45,Chest: 45,Arm Length: 45,Button Color: Blue.</span>
-                        <span className="card__description">Fabric: cotton</span>
-                        <span className="card__description">Suit Price: 999$</span>
-                        <Link to="add-to-cart">
-                        <img src="/shopping-cart.svg" alt="cart" className="readysuit-cart" />
-                        </Link>
-                    </div>
-                </article>
-                <article className="card__article">
-                    <img src="/doublebreast3.png" alt="image" className="card__img" />
-
-                    <div className="card__data">
-                    <h2 className="card__title">Doublebreast Suit</h2>
-                        <span className="card__description">Suit measurements:</span>
-                        <span className="card__description">Length: 45,Waist: 45,Chest: 45,Arm Length: 45,Button Color: Blue.</span>
-                        <span className="card__description">Fabric: cotton</span>
-                        <span className="card__description">Suit Price: 999$</span>
-                        <Link to="add-to-cart">
-                        <img src="/shopping-cart.svg" alt="cart" className="readysuit-cart" />
-                        </Link>
-                    </div>
-                </article>
-                <article className="card__article">
-                    <img src="/doublebreast4.png" alt="image" className="card__img" />
-
-                    <div className="card__data">
-                    <h2 className="card__title">Doublebreast Suit</h2>
-                        <span className="card__description">Suit measurements:</span>
-                        <span className="card__description">Length: 45,Waist: 45,Chest: 45,Arm Length: 45,Button Color: Blue.</span>
-                        <span className="card__description">Fabric: cotton</span>
-                        <span className="card__description">Suit Price: 999$</span>
-                        <Link to="add-to-cart">
-                        <img src="/shopping-cart.svg" alt="cart" className="readysuit-cart" />
-                        </Link>
-                    </div>
-                </article>
-                <article className="card__article">
-                    <img src="/doublebreast5.png" alt="image" className="card__img" />
-
-                    <div className="card__data">
-                    <h2 className="card__title">Doublebreast Suit</h2>
-                        <span className="card__description">Suit measurements:</span>
-                        <span className="card__description">Length: 45,Waist: 45,Chest: 45,Arm Length: 45,Button Color: Blue.</span>
-                        <span className="card__description">Fabric: cotton</span>
-                        <span className="card__description">Suit Price: 999$</span>
-                        <Link to="add-to-cart">
-                        <img src="/shopping-cart.svg" alt="cart" className="readysuit-cart" />
-                        </Link>
-                    </div>
-                </article> */}
             </div>
-            {/* <div className="pagination"> */}
-                {/* Pagination buttons */}
-                {/* {[...Array(totalPages).keys()].map(page => (
+        {/* Pagination buttons */}
+            {/* <div className="pagination">
+                {[...Array(totalPages).keys()].map(page => (
                     <button 
                         key={page+1} 
                         onClick={() => handlePageChange(page+1)} 
